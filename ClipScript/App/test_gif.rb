c69a15edd6a7bf458f9369b25ab57ca2a1fdafe2
ongaeshi <ongaeshi0621@gmail.com>
2021-04-30 15:04:34 +0900

    test: gifとテキストファイルを連動させる

--- ClipScript/App/test_gif.rb
+++ ClipScript/App/test_gif.rb
@@ -1,11 +1,28 @@
 require 'clip'
 
 test_gif = GifReader.new("example/test.gif")
+font = Font.new(50)
 
 script do |c|
   Drawer.background("white")
   g = c.gif(test_gif)
   g.play
+end
+
+W = 0.5
+
+script do |c|
+  t = c.text(font, 0, 200, color: "orange")
+  loop do
+    t.text = "GIFアニメを再生中"
+    c.wait W
+    t.text += "."
+    c.wait W
+    t.text += "."
+    c.wait W
+    t.text += "🎃"
+    c.wait W
+  end
 end
 
 App.run
