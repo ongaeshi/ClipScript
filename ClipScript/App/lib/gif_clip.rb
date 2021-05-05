f1749d43286ff84352f29ea4a756024fe20ffd47
ongaeshi <ongaeshi0621@gmail.com>
2021-05-05 16:26:28 +0900

    feat: GifClipのAPI名整理、再生位置を変更可能に

--- ClipScript/App/lib/gif_clip.rb
+++ ClipScript/App/lib/gif_clip.rb
@@ -3,35 +3,33 @@ require 'drawer'
 
 module Clip
   class GifClip < ClipObject
-    attr_reader :rate
+    attr_accessor :current_time, :playback_rate
 
     def initialize(parent, reader, opt = {})
       super(parent)
       @reader = reader  # Need gc protection for images
       @textures = @reader.textures
-      @gif_time = 0
-      @play_rate = 1
-      @rate = 1
+      @current_time = 0
+      @playback_rate = 1
+      @is_play = true
 
       set_script do
         loop do
-          @gif_time += min_delta_time * @play_rate
+          if @is_play
+            @current_time += min_delta_time * @playback_rate
+          end
           wait_delta
         end
       end
     end
 
     def draw
-      i = @reader.index(@gif_time)
+      i = @reader.index(@current_time)
       Drawer.texture(@textures[i], 0, 0)
     end
 
-    def play = @play_rate = @rate
-    def stop = @play_rate = 0.0
-
-    def rate=(v)
-      @play_rate = @rate = v
-    end
+    def play = @is_play = true
+    def stop = @is_play = false
   end
 
   class ClipObject
--- ClipScript/App/test_gif.rb
+++ ClipScript/App/test_gif.rb
@@ -1,9 +1,9 @@
 require 'clip'
 
 test_gif = GifReader.new("example/test.gif")
-font = Font.new(40)
+font = Font.new(30)
 
-App.end_time = test_gif.duration * 2
+App.end_time = test_gif.duration * 3 + 0.4
 
 script do |c|
   Drawer.background("white")
@@ -13,8 +13,11 @@ script do |c|
   g = c.gif(test_gif)
   t = c.text(font, 10, 200, color: "orange")
 
+  t_center = c.text(font, 10, 70, color: [32, 32, 32, 32])
+  t_center.text = "test.gif"
+
   1.upto(2) do
-    t.text = "GIFアニメを再生中"
+    t.text = "再生中"
     1.upto(3) do
       t.text += "."
       c.wait 0.4
@@ -22,18 +25,29 @@ script do |c|
   end
   c.until_time 2.4
 
-  t.text = "GIFアニメを停止🎃"
+  t.text = "停止🎃"
   g.stop
   c.until_time 3.6
 
   g.play
   1.upto(4) do
     t.text = "4倍速"
-    g.rate = 4
+    g.playback_rate = 4
     1.upto(3) do
       t.text += "."
       c.wait 0.1
     end
+  end
+  c.until_time 4.8
+
+  # stop motion
+  g.stop
+  current_time = 0.0
+  1.upto(7) do
+    g.current_time = current_time
+    t.text = "t = #{current_time}"
+    c.wait 0.4
+    current_time += 0.4
   end
 end
 
