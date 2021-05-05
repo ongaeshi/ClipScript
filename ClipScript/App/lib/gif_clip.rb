238ed9227a7a77dfb5a6e6b97eb578f8067ff769
ongaeshi <ongaeshi0621@gmail.com>
2021-05-05 16:01:14 +0900

    feat: GifClip#rateを実装

--- ClipScript/App/lib/gif_clip.rb
+++ ClipScript/App/lib/gif_clip.rb
@@ -3,16 +3,19 @@ require 'drawer'
 
 module Clip
   class GifClip < ClipObject
+    attr_reader :rate
+
     def initialize(parent, reader, opt = {})
       super(parent)
       @reader = reader  # Need gc protection for images
       @textures = @reader.textures
       @gif_time = 0
+      @play_rate = 1
       @rate = 1
 
       set_script do
         loop do
-          @gif_time += min_delta_time * @rate
+          @gif_time += min_delta_time * @play_rate
           wait_delta
         end
       end
@@ -23,8 +26,12 @@ module Clip
       Drawer.texture(@textures[i], 0, 0)
     end
 
-    def play = @rate = 1.0
-    def stop = @rate = 0.0
+    def play = @play_rate = @rate
+    def stop = @play_rate = 0.0
+
+    def rate=(v)
+      @play_rate = @rate = v
+    end
   end
 
   class ClipObject
--- ClipScript/App/test_gif.rb
+++ ClipScript/App/test_gif.rb
@@ -3,26 +3,25 @@ require 'clip'
 test_gif = GifReader.new("example/test.gif")
 font = Font.new(40)
 
+App.end_time = test_gif.duration * 2
+
 script do |c|
   Drawer.background("white")
 end
-
-W = 0.5
 
 script do |c|
   g = c.gif(test_gif)
   t = c.text(font, 10, 250, color: "orange")
   loop do
     t.text = "GIFアニメを再生中"
-    c.wait W
-    t.text += "."
-    c.wait W
-    t.text += "."
-    c.wait W
+    c.wait 2.4
     t.text = "GIFアニメを停止🎃"
     g.stop
-    c.wait W
+    c.wait 1.2
     g.play
+    t.text = "4倍速"
+    g.rate = 4
+    c.wait 1.2
   end
 end
 
