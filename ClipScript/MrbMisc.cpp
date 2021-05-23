3c639437a5fef7d6cdfa24e6adafdede5d7a1644
ongaeshi <ongaeshi0621@gmail.com>
2021-05-24 00:30:07 +0900

    feat カーソル位置も表示

--- ClipScript/MrbMisc.cpp
+++ ClipScript/MrbMisc.cpp
@@ -160,21 +160,22 @@ mrb_value timeline_ui(mrb_state* mrb, mrb_value self)
 
     auto button = !is_stop ? U"▶" : U"⏹️";
 
-    const auto UiHeight = 100;
+    const auto UiHeight = 60;
     const auto UiPosY = Scene::Height() - UiHeight;
-    const auto Margin = 50;
+    const auto UiOffset = 10;
 
     Rect(0, UiPosY, 800, UiHeight).draw(ColorF(0.8, 0.9, 1.0));
 
-    if (SimpleGUI::Button(button, Vec2(20, UiPosY + Margin))) {
+    if (SimpleGUI::Button(button, Vec2(20, UiPosY + UiOffset))) {
         is_stop = !is_stop;
     }
 
-    if (SimpleGUI::Slider(time, 0.0, end_time, Vec2(180, UiPosY + Margin), 600)) {
+    if (SimpleGUI::Slider(time, 0.0, end_time, Vec2(180, UiPosY + UiOffset), 500)) {
         is_stop = true;
     }
 
-    font(U"{:3.2f}"_fmt(time)).draw(100, UiPosY + Margin, Palette::Black);
+    font(U"{:3.2f}"_fmt(time)).draw(100, UiPosY + UiOffset, Palette::Black);
+    font(Cursor::Pos()).draw(690, UiPosY + UiOffset, Palette::Black);
 
     mrb_value array = mrb_ary_new(mrb);
     mrb_ary_push(mrb, array, mrb_float_value(mrb, time));
