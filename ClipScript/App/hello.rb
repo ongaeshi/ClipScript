2b02236276212b55a532eabe43b17edb63db6444
ongaeshi <ongaeshi0621@gmail.com>
2021-08-02 22:52:07 +0900

    feat: t.text.lengthを使うように

--- ClipScript/App/hello.rb
+++ ClipScript/App/hello.rb
@@ -1,4 +1,4 @@
-require 'clip'
+require "clip"
 
 App.window_size(400, 225)
 
@@ -17,34 +17,18 @@ script do |root|
       end
     end
   end
-end  
+end
 
 script do |root|
-  t = root.text(font, 200, 30, color: "black", text: "Hello, World!", length: 0, center: true)
+  t = root.text(font, 200, 100, color: "black", text: "Hello, World!", length: 0, center: true)
   root.wait 0.2
 
-  1.upto(7) do
+  1.upto(t.text.length) do
     t.length += 1
     root.wait 0.1
   end
 
-  root.wait 0.3
-
-  1.upto(6) do
-    t.length += 1
-    root.wait 0.08
-  end
-
-  root.wait 0.3
-
-  t2 = root.text(font_s, 200, 110, color: "blue", text: "こんにちは、世界！", length: 0, center: true)
-
-  1.upto(t2.text.length) do
-    t2.length += 1
-    root.wait 0.1
-  end
-
-  root.until_time 3.5
+  root.until_time 3
 
   x = root.texture(smile, 180, 150)
   x.scale(0.4, 0.4)
