1c51c0156974945dc8bb8d7e3efb2fd53fc9989e
ongaeshi <ongaeshi0621@gmail.com>
2021-11-03 11:58:41 +0900

    Update MrbImage.hpp

--- ClipScript/MrbImage.hpp
+++ ClipScript/MrbImage.hpp
@@ -1,1 +1,17 @@
+#pragma once
 
+#include "MrbObject.hpp"
+
+//----------------------------------------------------------
+namespace siv3druby {
+class MrbImage
+    : public MrbObject<Texture>
+{
+public:
+    static void Init(mrb_state *mrb);
+
+private:
+    static mrb_value initialize(mrb_state *mrb, mrb_value self);
+    static mrb_value fill(mrb_state *mrb, mrb_value self);
+};
+}
